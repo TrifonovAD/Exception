e80fe48ff6de9e9f09fe25f43cca0f0110bd2fb4
TrifonovAD <trifonov76@gmail.com>
2023-07-27 01:20:24 +0300

    Add homework 2 task 1.

---
+++ src/Homework2/Task1.java
@@ -0,0 +1,9 @@
+package Homework2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        FloatInput floatInput = new FloatInput();
+        float number = floatInput.readFloatFromUser();
+        System.out.println("User input number: " + number);
+    }
+}
